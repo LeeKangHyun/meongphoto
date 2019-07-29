3406c8d9782a64c0a18090a06308b2db090f1f34
kanghyunlee <rkdgusdl92@gmail.com>
2019-07-29 23:49:38 +0900

    Font Asset
    폰트 추가

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  "rules": {
+    "prettier/prettier": 0,
+    "semi": 0
+  },
+  "env": {
+    "browser": true,
+    "node": true
+  }
+};
